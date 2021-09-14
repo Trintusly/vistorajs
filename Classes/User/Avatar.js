54876a6ed2133c8087aa11b9e6793abfe124362a
Danyal I <mdanyal28@outlook.com>
2021-09-14 15:39:01 +0500

    yes

--- Classes/User/Avatar.js
+++ Classes/User/Avatar.js
@@ -4,17 +4,10 @@ class Avatar {
 
     constructor() {
         this.colors = {
-            head:      null,
-            torso:     null,
-            right_arm: null,
-            left_arm:  null,
-            right_leg: null,
-            left_leg:  null
+            head: null, torso: null, right_arm: null, left_arm:  null, right_leg: null, left_leg:  null
         }
         this.hats  = {
-            hat1: 0,
-            hat2: 0,
-            hat3: 0
+            hat1: 0, hat2: 0, hat3: 0
         }
         this.face  = 0
         this.shirt = 0
